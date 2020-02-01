6bbfb197b30d6507961657d4f24afe838f673e86
guy.golan <guygolan91@gmail.com>
2020-02-01 17:40:22 +0200

    converting InvoiceCard to functional

--- react-tutorial-code/src/components/InvoiceCard.js
+++ react-tutorial-code/src/components/InvoiceCard.js
@@ -1,17 +1,13 @@
-import React from 'react';
+import React from "react";
 import "./InvoiceCard.css";
-class InvoiceCard extends React.Component{
+const InvoiceCard = props => (
+    <div className="invoice-card">
+        <div><b>Invoice Id: </b>{props.id}</div>
+        <div><b>Amount: </b>{props.amount}{props.currency}</div>
+        <div><b>Date Created: </b>{props.dateCreated}</div>
+        <div><b>Account Id: </b>{props.accountId}</div>
+    </div>
+);
 
-    render(){
-        return (
-            <div className="invoice-card">
-                <div><b>Invoice Id: </b>{this.props.id}</div>
-                <div><b>Amount: </b>{this.props.amount}{this.props.currency}</div>
-                <div><b>Date Created: </b>{this.props.dateCreated}</div>
-                <div><b>Account Id: </b>{this.props.accountId}</div>
-            </div>
-        );
-    }
-}
 // usage: <InvoiceCard id="30112354" amount={15} accountId="3579513" currency="$" dateCreated={new Date().toString()} />
 export default InvoiceCard;
