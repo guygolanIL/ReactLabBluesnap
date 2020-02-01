fb56166771e497ca30e494585305e938626fd1fa
guy.golan <guygolan91@gmail.com>
2020-02-01 15:59:11 +0200

    changed app to class based comp

--- react-tutorial-code/src/App.js
+++ react-tutorial-code/src/App.js
@@ -3,6 +3,8 @@ import InvoiceCard from './components/Invoice';
 import './App.css';
 
 class App extends React.Component {
+
+  
   render(){
     return (
       <InvoiceCard number="3000002" amount={15} currency='$' dateCreated={new Date().toString()}></InvoiceCard>
