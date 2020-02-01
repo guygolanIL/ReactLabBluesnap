e64473f80123d3f3d38a6f655ff761163f19ecff
guy.golan <guy.golan@bluesnap.com>
2020-02-01 20:16:22 +0200

    removed unnecessary stuf

--- react-tutorial-code/src/App.js
+++ react-tutorial-code/src/App.js
@@ -1,11 +1,9 @@
 import React from 'react';
-import Invoice from './components/Invoice';
-import './App.css';
 
 function App() {
 
   return (
-    <Invoice number="3000002" dateCreated={new Date()}></Invoice>
+    <h1>Hello App</h1>
   );
 }
 
