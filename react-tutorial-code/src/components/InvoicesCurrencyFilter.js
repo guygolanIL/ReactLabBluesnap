6ce4da5e38123bbf7e86166a1399a3f83dfc8c9f
guy.golan <guygolan91@gmail.com>
2020-02-04 22:46:13 +0200

    currencyFilter

---
+++ react-tutorial-code/src/components/InvoicesCurrencyFilter.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+export const InvoicesCurrencyFilter = props => {
+    return (
+        <div>
+            <select 
+                onChange={(e) => {
+                    props.onSelected(e.target.value)
+                }}>
+                <option value="USD">USD</option>
+                <option value="ILS">ILS</option>
+                <option value="EUR">EUR</option>
+            </select>
+        </div>
+    );
+};
--- react-tutorial-code/src/components/InvoicesList.js
+++ react-tutorial-code/src/components/InvoicesList.js
@@ -1,66 +1,35 @@
 import React from "react";
 import "./InvoicesList.css";
 import InvoiceCard from "./InvoiceCard";
-import { InvoicesFilter } from "./InvoicesFilter";
+import {InvoicesCurrencyFilter } from "./InvoicesCurrencyFilter";
+
 class InvoicesList extends React.Component {
     state = {
-        filter: null,
-        loadingData: false,
-        invoices: []
+        currency: null,
+        invoices: 
+            [
+                {
+                    id: "3216547",
+                    accountId: "264578",
+                    amount: 6,
+                    currency: "USD",
+                    dateCreated: new Date()
+                },
+                {
+                    id: "3216123",
+                    accountId: "264578",
+                    amount: 12,
+                    currency: "ILS",
+                    dateCreated: new Date()
+                }
+            ]
+        
     };
 
-    componentDidMount() {
-        this.fetchInvoices();
-    }
-
-    fetchInvoices() {
-        this.setState({
-            loadingData: true
-        });
-
-        setTimeout(() => {
-            console.log("fetching data");
-            this.setState({
-                loadingData: false,
-                invoices: [
-                    {
-                        id: "3216547",
-                        accountId: "264578",
-                        amount: 6,
-                        currency: "USD",
-                        dateCreated: new Date()
-                    },
-                    {
-                        id: "3216123",
-                        accountId: "264578",
-                        amount: 12,
-                        currency: "ILS",
-                        dateCreated: new Date()
-                    },
-                    {
-                        id: "3214789",
-                        accountId: "264551",
-                        amount: 1,
-                        currency: "EUR",
-                        dateCreated: new Date()
-                    }
-                ]
-            });
-        }, 1500);
-    }
-
-    getInvoicesCards() {
-        return this.state.invoices
-            .filter(invoice => {
-                if (this.state.filter) {
-                    return (
-                        invoice[this.state.filter.key] ===
-                        this.state.filter.value
-                    );
-                } else {
-                    return true;
-                }
-            })
+    render() {
+        const filteredInvoices = (
+            this.state.invoices
+            .filter(invoice => invoice.currency === this.state.currency)
             .map(invoice => (
                 <InvoiceCard
                     key={invoice.id}
@@ -70,31 +39,17 @@ class InvoicesList extends React.Component {
                     currency={invoice.currency}
                     dateCreated={invoice.dateCreated.toString()}
                 />
-            ));
-    }
+            ))
+        );
 
-    render() {
         return (
             <div className="InvoicesList">
-                <h1
-                    style={{
-                        textAlign: "center"
-                    }}
-                >
-                    Invoices
-                </h1>
-                <InvoicesFilter
-                    onFilter={filter => {
-                        this.setState({
-                            filter
-                        });
-                    }}
-                    onResetFilters={() => {
-                        this.setState({ filter: null });
+                <InvoicesCurrencyFilter
+                    onSelected={(currency) => {
+                        this.setState({ currency: currency })
                     }}
                 />
-                {this.state.loadingData ? "Loading..." : null}
-                {this.getInvoicesCards()}
+                {filteredInvoices}
             </div>
         );
     }
