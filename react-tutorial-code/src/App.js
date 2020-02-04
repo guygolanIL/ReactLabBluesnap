082f3054b73048d97f3a7d4cdb540c51222a8e23
guy.golan <guygolan91@gmail.com>
2020-02-04 21:27:51 +0200

    new state component

--- react-tutorial-code/src/App.js
+++ react-tutorial-code/src/App.js
@@ -1,10 +1,23 @@
 import React from "react";
 import "./App.css";
 import InvoicesList from "./components/InvoicesList";
+import InvoiceCard from "./components/InvoiceCard";
 
 class App extends React.Component {
+
+  state = {
+    showInvoice: false
+  };
+
   render() {
-      return <InvoicesList/>;
+      return (
+
+        <div>
+          <button onClick={() => this.setState({ showInvoice: !this.state.showInvoice })}>Toggle hidden</button>
+          {this.state.showInvoice ? <InvoiceCard id='123456' amount={15} currency="$" accountId="123" dateCreated={new Date().toString()}/> : "Hiding Invoice"}
+        </div>
+        
+      );
   }
 }
 
