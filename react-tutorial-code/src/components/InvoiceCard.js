f90fc1aa6cb6b3a97f4621fdf5311eee8ba85b62
guy.golan <guygolan91@gmail.com>
2020-02-10 19:47:20 +0200

    asd

--- react-tutorial-code/src/components/InvoiceCard.js
+++ react-tutorial-code/src/components/InvoiceCard.js
@@ -1,20 +1,25 @@
 import React from "react";
 import "./InvoiceCard.css";
 class InvoiceCard extends React.Component {
-    render(){
+    render() {
         return (
             <div className="invoice-card">
                 <div><b>id: </b>{this.props.id}</div>
                 <div><b>amount: </b>{this.props.amount}{this.props.currency}</div>
                 <div><b>date created: </b>{this.props.dateCreated}</div>
                 <div><b>account id: </b>{this.props.accountId}</div>
-
+                
                 <ul>
-                    {this.props.contractCharges.map(contractCharge => <li key={contractCharge.id}>Contract Charge ID: {contractCharge.id}, Charge Amount: {contractCharge.chargeAmount}</li>)}
+                    {this.props.contractCharges.map(contractCharge => (
+                        <li key={contractCharge.id}>
+                            Contract Charge ID: {contractCharge.id}, Charge
+                            Amount: {contractCharge.chargeAmount}
+                        </li>
+                    ))}
                 </ul>
             </div>
         );
-    }    
+    }
 }
 
 // usage: <InvoiceCard id="30112354" amount={15} accountId="3579513" currency="$" dateCreated={new Date().toString()} />
